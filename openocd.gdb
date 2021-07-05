977792e87f8a64c28250abd06f4a0004c91fb473
Kacper Leśniański <kacper.lesnianski@wp.pl>
2021-07-05 18:56:53 +0200

    updatefiles

--- openocd.gdb
+++ openocd.gdb
@@ -1,4 +1,4 @@
-#target extended-remote :3333
+target extended-remote :3333
 
 # print demangled symbols
 set print asm-demangle on
@@ -6,9 +6,9 @@ set print asm-demangle on
 load
 
 # detect unhandled exceptions, hard faults and panics
-break DefaultHandler
-break HardFault
-break rust_begin_unwind
+#break DefaultHandler
+#break HardFault
+#break rust_begin_unwind
 
 monitor arm semihosting enable
 
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,25 @@
 #![no_std]
 #![no_main]
 
+use cortex_m::peripheral::NVIC;
 use cortex_m_rt::entry;
+use cortex_m_semihosting::hprintln;
 use stm32f1xx_hal::{prelude::*, stm32};
+use stm32f1xx_hal::pac::{Interrupt, interrupt};
+use stm32f1xx_hal::usb::{Peripheral, UsbBus, UsbBusType};
 use stm32f1xx_hal::pac::Peripherals;
+
+use usb_device::{bus::UsbBusAllocator, prelude::*};
+use usbd_serial::{SerialPort, USB_CLASS_CDC};
 
 use cortex_m::asm::{delay, wfi};
 use embedded_hal::digital::v2::OutputPin;
 
 extern crate panic_semihosting;
+
+static mut USB_BUS: Option<UsbBusAllocator<UsbBusType>> = None;
+static mut USB_SERIAL: Option<usbd_serial::SerialPort<UsbBusType>> = None;
+static mut USB_DEVICE: Option<UsbDevice<UsbBusType>> = None;
 
 #[entry]
 fn main() -> ! {
@@ -29,10 +40,83 @@ fn main() -> ! {
 
     let mut led = gpioc.pc13.into_push_pull_output(&mut gpioc.crh);
    
+    let mut gpioa = dp.GPIOA.split(&mut rcc.apb2);
+
+    // BluePill board has a pull-up resistor on the D+ line.
+    // Pull the D+ pin down to send a RESET condition to the USB bus.
+    // This forced reset is needed only for development, without it host
+    // will not reset your device when you upload new firmware.
+    let mut usb_dp = gpioa.pa12.into_push_pull_output(&mut gpioa.crh);
+    usb_dp.set_low().unwrap();
+    delay(clocks.sysclk().0 / 100);
+
+    let usb_dm = gpioa.pa11;
+    let usb_dp = usb_dp.into_floating_input(&mut gpioa.crh);
+
+    let usb = Peripheral {
+        usb: dp.USB,
+        pin_dm: usb_dm,
+        pin_dp: usb_dp,
+    };
+
+    // Unsafe to allow access to static variables
+    unsafe {
+        let bus = UsbBus::new(usb);
+
+        USB_BUS = Some(bus);
+
+        USB_SERIAL = Some(SerialPort::new(USB_BUS.as_ref().unwrap()));
+
+        let usb_dev = UsbDeviceBuilder::new(USB_BUS.as_ref().unwrap(), UsbVidPid(0x16c0, 0x27dd))
+            .manufacturer("Fake company")
+            .product("Serial port")
+            .serial_number("TEST")
+            .device_class(USB_CLASS_CDC)
+            .build();
+
+        USB_DEVICE = Some(usb_dev);
+    }
+
+    unsafe{
+        NVIC::unmask(Interrupt::USB_HP_CAN_TX);
+        NVIC::unmask(Interrupt::USB_LP_CAN_RX0);
+    }
+    
     loop {
         led.set_high().unwrap();
         delay(clocks.sysclk().0 / 10);
         led.set_low().unwrap();
         delay(clocks.sysclk().0 / 10);
+    }
+}
+
+#[interrupt]
+fn USB_HP_CAN_TX() {
+    usb_interrupt();
+}
+
+#[interrupt]
+fn USB_LP_CAN_RX0() {
+    usb_interrupt();
+}
+
+fn usb_interrupt() {
+    let usb_dev = unsafe { USB_DEVICE.as_mut().unwrap() };
+    let serial = unsafe { USB_SERIAL.as_mut().unwrap() };
+
+    if !usb_dev.poll(&mut [serial]) {
+        return;
+    }
+
+    let mut buf = [0u8; 72];
+
+    match serial.read(&mut buf) {
+        Ok(count) if count > 0 => {
+            // Echo back in upper case
+
+            serial.write(&buf).ok();
+            hprintln!("{:?}", buf);
+        }
+        _ => {}
     }
 }
