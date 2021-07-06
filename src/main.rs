2046875d6827aa1d02bae0b0d9dfddeb7b30013f
Kacper Leśniański <kacper.lesnianski@wp.pl>
2021-07-06 20:25:50 +0200

    Configure matrix pins

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
 #![no_std]
 #![no_main]
+#![feature(const_generics)]
 
 mod display;
 
 use cortex_m::peripheral::NVIC;
 use cortex_m_rt::entry;
 use cortex_m_semihosting::hprintln;
-use stm32f1xx_hal::{prelude::*, stm32};
-use stm32f1xx_hal::pac::{Interrupt, interrupt};
-use stm32f1xx_hal::usb::{Peripheral, UsbBus, UsbBusType};
+use hub75::{Hub75, Pins};
 use stm32f1xx_hal::pac::Peripherals;
+use stm32f1xx_hal::pac::{interrupt, Interrupt};
+use stm32f1xx_hal::usb::{Peripheral, UsbBus, UsbBusType};
+use stm32f1xx_hal::{prelude::*, stm32};
 
 use usb_device::{bus::UsbBusAllocator, prelude::*};
 use usbd_serial::{SerialPort, USB_CLASS_CDC};
@@ -22,6 +24,25 @@ extern crate panic_semihosting;
 static mut USB_BUS: Option<UsbBusAllocator<UsbBusType>> = None;
 static mut USB_SERIAL: Option<usbd_serial::SerialPort<UsbBusType>> = None;
 static mut USB_DEVICE: Option<UsbDevice<UsbBusType>> = None;
+
+const DOUBLE_SCREEN_WIDTH: usize = 128;
+
+const PIN_POS: Pins = Pins {
+    r1: 0,
+    g1: 1,
+    b1: 5,
+    r2: 6,
+    g2: 7,
+    b2: 8,
+    a: 9,
+    b: 10,
+    c: 11,
+    clock: 12,
+    latch: 13,
+    oe: 14,
+};
+
+static mut DISPLAY: Option<Hub75<PIN_POS, DOUBLE_SCREEN_WIDTH>> = None;
 
 #[entry]
 fn main() -> ! {
@@ -41,8 +62,23 @@ fn main() -> ! {
     let mut gpioc = dp.GPIOC.split(&mut rcc.apb2);
 
     let mut led = gpioc.pc13.into_push_pull_output(&mut gpioc.crh);
-   
+
     let mut gpioa = dp.GPIOA.split(&mut rcc.apb2);
+    let mut gpiob = dp.GPIOB.split(&mut rcc.apb2);
+    
+    //Matrix pins
+    let mut _r1 = gpiob.pb0.into_push_pull_output(&mut gpiob.crl);
+    let mut _g1 = gpiob.pb1.into_push_pull_output(&mut gpiob.crl);
+    let mut _b1 = gpiob.pb5.into_push_pull_output(&mut gpiob.crl);
+    let mut _r2 = gpiob.pb6.into_push_pull_output(&mut gpiob.crl);
+    let mut _b2 = gpiob.pb7.into_push_pull_output(&mut gpiob.crl);
+    let mut _g2 = gpiob.pb8.into_push_pull_output(&mut gpiob.crh);
+    let mut _a = gpiob.pb9.into_push_pull_output(&mut gpiob.crh);
+    let mut _b = gpiob.pb10.into_push_pull_output(&mut gpiob.crh);
+    let mut _c = gpiob.pb11.into_push_pull_output(&mut gpiob.crh);
+    let mut _clock = gpiob.pb12.into_push_pull_output(&mut gpiob.crh);
+    let mut _latch = gpiob.pb13.into_push_pull_output(&mut gpiob.crh);
+    let mut _oe = gpiob.pb14.into_push_pull_output(&mut gpiob.crh);
 
     // BluePill board has a pull-up resistor on the D+ line.
     // Pull the D+ pin down to send a RESET condition to the USB bus.
@@ -79,11 +115,11 @@ fn main() -> ! {
         USB_DEVICE = Some(usb_dev);
     }
 
-    unsafe{
+    unsafe {
         NVIC::unmask(Interrupt::USB_HP_CAN_TX);
         NVIC::unmask(Interrupt::USB_LP_CAN_RX0);
     }
-    
+
     loop {
         led.set_high().unwrap();
         delay(clocks.sysclk().0 / 10);
