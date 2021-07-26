87291d226313de5f25a6e20dff3b5335ae5822d3
Kacper Leśniański <kacper.lesnianski@wp.pl>
2021-07-26 20:48:46 +0200

    Add error messages

--- src/display.rs
+++ src/display.rs
@@ -13,6 +13,18 @@ pub enum DisplayError{
     DrawError,
 }
 
+impl DisplayError{
+    pub fn message(&self) -> &'static str{
+        match self {
+            DisplayError::OutOfBounds => "Index out of Bounds",
+            DisplayError::IncorrectMode => "Incorrect Mode",
+            DisplayError::InvalidSetting => "Invalid Setting",
+            DisplayError::InvalidCommand => "Invalid Command",
+            DisplayError::DrawError => "Drawing Error",
+        }
+    }
+}
+
 pub enum DisplayMode<'a, const MAX_ROW_LENGTH: usize>{
     TextMode(TextDisplay<'a, MAX_ROW_LENGTH>),
     DirectMode
